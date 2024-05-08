2f9e108b71ff89a27c3a588b448393e049ddbf39
sumeyya-arslan <89263842+sumeyya-arslan@users.noreply.github.com>
2024-05-08 08:04:33 +0300

    9999'dan 0'a 1 sn aralıklarla ekrana yazdırma

--- zaman rölesi 4 digit 7segment display(25.04.2024).X/display.c
+++ zaman rölesi 4 digit 7segment display(25.04.2024).X/display.c
@@ -1,35 +1,39 @@
 #include <xc.h>
 #include <stdint.h>
 
-#pragma config FOSC = HS     
-#pragma config WDTE = OFF     
-#pragma config PWRTE = OFF    
-#pragma config MCLRE = OFF    
-#pragma config CP = OFF      
-#pragma config BOREN = ON    
-#pragma config CLKOUTEN = OFF 
-#pragma config WRT = OFF    
+// Konfigürasyon bitleri
+#pragma config FOSC = HS
+#pragma config WDTE = OFF
+#pragma config PWRTE = OFF
+#pragma config MCLRE = OFF
+#pragma config CP = OFF
+#pragma config BOREN = ON
+#pragma config CLKOUTEN = OFF
+#pragma config WRT = OFF
 
-#define _XTAL_FREQ 4000000  
+#define _XTAL_FREQ 4000000
 
-#define Dgt1 LATCbits.LATC3  
-#define Dgt2 LATCbits.LATC4  
-#define DS  LATC6      //PORTCbits.RC6
-#define STCP LATC5     //PORTCbits.RC5
-#define SHCP LATC7     //PORTCbits.RC7
+// Pin tan?mlar?
+#define Dgt1 LATCbits.LATC3
+#define Dgt2 LATCbits.LATC4
+#define DS LATC6
+#define STCP LATC5
+#define SHCP LATC7
 
- unsigned int number;
- unsigned char d1, d2, d3, d4;
-unsigned char Digit[10] = { 0x03, 0x9F, 0X25, 0x0D, 0x99, 0x49, 0x41, 0x1F, 0x01, 0x09 };// SIRASIYLA 0,1,2,3,4,5,6,7,8,9
+unsigned char d1, d2, d3, d4;
+unsigned char Digit[10] = {0x03, 0x9F, 0X25, 0x0D, 0x99, 0x49, 0x41, 0x1F, 0x01, 0x09}; // 0-9 aras?ndaki rakamlar?n kar??l?k gelen de?erleri
 
+volatile unsigned int number = 9999; // 9999 ile ba?la
 
-void writeDigit(unsigned char digit) {
+void writeDigit(unsigned char digit)
+{
     unsigned char i;
     SHCP = 0;
     STCP = 0;
     SHCP = 0;
     STCP = 0;
-    for (i = 0; i < 8; i++) {
+    for (i = 0; i < 8; i++)
+    {
         DS = (digit >> i) & 0x01;
         SHCP = 1;
         SHCP = 1;
@@ -40,8 +44,8 @@ void writeDigit(unsigned char digit) {
     STCP = 1;
 }
 
-void displayNumber(unsigned int number1) {
-   
+void displayNumber(unsigned int number1)
+{
     d1 = number1 / 1000;
     number1 %= 1000;
     d2 = number1 / 100;
@@ -49,52 +53,62 @@ void displayNumber(unsigned int number1) {
     d3 = number1 / 10;
     d4 = number1 % 10;
 
-    /*d1=1;
-    d2=2;
-    d3=3;
-    d4=4;*/
-
-    // Display each digit
-    //digit_select(4);
-    Dgt1 = 1;//a1=digit1
-            Dgt2 = 1;
+    // Her basama?? göster
+    Dgt1 = 1; // Basamak 1'i seç
+    Dgt2 = 1;
     writeDigit(Digit[d1]);
-   __delay_ms(50);
+     __delay_ms(10);
 
-    //digit_select(3);
-    Dgt1 = 0;//a2=digit2
-            Dgt2 = 1;
+    Dgt1 = 0; // Basamak 2'yi seç
+    Dgt2 = 1;
     writeDigit(Digit[d2]);
-   __delay_ms(50);
+     __delay_ms(10);
 
-    //digit_select(2);
-    Dgt1 = 1;//a3=digit3
-            Dgt2 = 0;
+    Dgt1 = 1; // Basamak 3'ü seç
+    Dgt2 = 0;
     writeDigit(Digit[d3]);
-    __delay_ms(50);
+     __delay_ms(10);
 
-    //digit_select(1);
-    Dgt1 = 0;//a4=digit4
-            Dgt2 = 0;
+    Dgt1 = 0; // Basamak 4'ü seç
+    Dgt2 = 0;
     writeDigit(Digit[d4]);
-   __delay_ms(50);
+     __delay_ms(10);
+}
+
+void initTimer1()
+{
+    T1CONbits.TMR1CS = 0b00; // Timer1, FOSC/4 h?z?nda çal??acak ?ekilde ayarland?
+    T1CONbits.T1CKPS = 0b11; // Timer1 ön bölücü 1:8
+    TMR1 = 3036; // 1 saniyelik zaman dilimi için Timer1'i ba?lat
+    T1CONbits.TMR1ON = 1; // Timer1'i etkinle?tir
+    TMR1IE = 1; // Timer1 kesmelerini etkinle?tir
+    PEIE = 1; // Periferik kesmeleri etkinle?tir
+    GIE = 1; // Genel kesmeleri etkinle?tir
+}
+
+void interrupt Timer1_ISR(void)
+{
+    if (TMR1IF)
+    {
+        TMR1IF = 0; // Timer1 kesme bayra??n? s?f?rla
+
+        number--; // Her 1 saniyede bir say?y? azalt
+        if (number < 0)
+            number = 9999; // S?f?ra ula??ld???nda geri dön
+    }
 }
 
 void main(void)
 {
-    TRISC = 0x00;
-    PORTC=0x00;
+    TRISC = 0x00; // PORTC'yi ç?k?? olarak ayarla
+    PORTC = 0x00; // PORTC'yi ba?lat
 
+    initTimer1(); // Timer1'i ba?lat
 
     while (1)
     {
-       
-        
-        for (number = 0; number < 10000; number++)
-        {
-            displayNumber(number);
-            __delay_ms(10);
-        }
+        displayNumber(number);
     }
 }
+
 
